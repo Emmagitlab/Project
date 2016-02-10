33eb741764197861bc33caa60a5f72445c81de02
Fangyu Lin <johnlin989@gmail.com>
2016-02-10 08:01:51 -0500

    Update Query1.java
    fix output results to be correct.

--- src/Query1.java
+++ src/Query1.java
@@ -59,28 +59,13 @@ public class Query1{
            tuple[3].equals("4") || tuple[3].equals("5") || 
            tuple[3].equals("6"))){
             word.set(token);
+            context.write(word, one);
         }
-        context.write(word, one);
 	// System.out.println(itr.toString());
       }
     }
   }
   
-//  public static class IntSumReducer 
-//       extends Reducer<Text,IntWritable,Text,IntWritable> {
-//    private IntWritable result = new IntWritable();
-//
-//    public void reduce(Text key, Iterable<IntWritable> values, 
-//                       Context context
-//                       ) throws IOException, InterruptedException {
-//      int sum = 0;
-//      for (IntWritable val : values) {
-//        sum += val.get();
-//      }
-//      result.set(sum);
-//      context.write(key, result);
-//    }
-//  }
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
@@ -94,8 +79,9 @@ public class Query1{
     // job.setCombinerClass(IntSumReducer.class);
     // job.setReducerClass(IntSumReducer.class);
     // job.setOutputKeyClass(Text.class);
-    // job.setNumReduceTasks(2);
-    job.setOutputValueClass(Text.class);
+    job.setNumReduceTasks(0);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
