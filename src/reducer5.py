118270fcaa88c36811f0d60ffe936f41e4b1a7d6
Fangyu Lin <johnlin989@gmail.com>
2016-02-13 20:57:08 -0500

    Create reducer5.py

---
+++ src/reducer5.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# this is the hw2 query5 reducer file.
+
+from operator import itemgetter
+import sys
+
+custName = None
+custID = "-1"
+count = 0
+code = "5"
+
+# input comes from STDIN
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+
+    # parse the input we got from mapper.py
+    tuples = line.split(',')
+
+    # convert count (currently a string) to int
+    # try:
+    #    count = int(count)
+    # except ValueError:
+        # count was not a number, so silently
+        # ignore/discard this line
+    #    continue
+
+    # this IF-switch only works because Hadoop sorts map output
+    # by key (here: word) before it is passed to the reducer
+
+    if len(tuples)==3:
+	if count!=0 and tuple[3]==code:
+		print '%s,%s,%s' % (custID, custName, count)
+	custID = tuple[0]
+	custName = tuple[1]
+	count = 0
+    else if len(tuples)==2 and tuples[0]==custID:
+	count+=1
+    
+    # if current_word == word:
+    #     current_count += count
+    # else:
+    #     if current_word:
+            # write result to STDOUT
+    #         print '%s\t%s' % (current_word, current_count)
+    #    current_count = count
+    #    current_word = word
+
+# do not forget to output the last word if needed!
+if count!=0:
+    print '%s,%s,%s' % (custID, custName, count)
