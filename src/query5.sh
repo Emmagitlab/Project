3af9b75aac695ec36a992cdc658288e7614c69b9
Emmagitlab <emma2014yang@hotmail.com>
2016-02-22 09:56:24 -0500

    hw2 query5

--- src/query5.sh
+++ src/query5.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 # this is query5.sh file to run the hadoop streaming
 
-mapfile="mapper5.py"
-redfile="reducer5.py"
-infile1="p1_dataset/Customers.csv"
-infile2="p1_dataset/Transactions.csv"
+mapfile="mapper.py"
+redfile="reducer.py"
+infile1="hw2/Customers.csv"
+infile2="hw2/Transactions.csv"
 outfile="hw2_q5_out"
 jarfile="/usr/share/hadoop/contrib/streaming/hadoop-streaming-1.2.1.jar"
 
 hadoop fs -rmr $outfile
 
-hadoop jar $jarfile -D mapred.reduce.tasks=6 -file $mapfile -mapper $mapfile -file $redfile -reducer $redfile -input $infile1 $infile2 -output $outfile
+hadoop jar $jarfile \
+-mapper mapper.py \
+-reducer reducer.py \
+-input $infile1 \
+-input $infile2 \
+-output $outfile \
+-file mapper.py \
+-file reducer.py
+#hadoop jar $jarfile -D mapred.reduce.tasks=6 -file $mapfile -mapper $mapfile -file $redfile -reducer $redfile -input $infile1 -input $infile2 -output $outfile -partitioner org.apache.hadoop.mapred.lib.KeyFieldBasedPartitioner -jobconf stream.map.output.field.separator=^ -jobconf stream.num.map.output.key.fields=4 -jobconf map.output.key.field.separator=^ -jobconf num.key.fields.for.partition=1
+
