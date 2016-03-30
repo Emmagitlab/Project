a2f4c5587886b62d3424b8d2725c263ed3709c04
Fangyu Lin <johnlin989@gmail.com>
2016-03-29 20:38:27 -0400

    Added files via upload

--- hw4/KMeansClustering.java
+++ hw4/KMeansClustering.java
@@ -15,241 +15,137 @@
 package org.apache.hadoop.examples;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URI;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.StringTokenizer;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.lib.ChainMapper;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
-import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.jobcontrol.ControlledJob;
-import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
-import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class KMeansClustering{
-    
-  public static class TokenizerMapper 
+  public static Map<Integer, Integer[]> seedpoints = new HashMap<Integer, Integer[]>();
+  public static int itrcount = 0;
+  public static boolean flagin = true;
+  public static boolean endflag = false;
+  
+  public static class KMeansMapper 
        extends Mapper<Object, Text, IntWritable, Text>{
     
     private IntWritable seedID = new IntWritable();
     private Text inpoint = new Text();
-    private final ArrayList<Integer[]> seedlist= new ArrayList<Integer[]>();
+//    private Map<Integer, Integer[]> seedpoints = new HashMap<Integer, Integer[]>();
+//    private final ArrayList<Integer[]> seedlist= new ArrayList<Integer[]>();
+//    private Map<Integer, ArrayList<Integer[]>> pointclust = new HashMap<Integer, ArrayList<Integer[]>>();
     
-    public void setup(Context context) throws IOException{
-        Path pt = new Path("p4_dataset/KSeed.csv");     //Location of file in HDFS
-        FileSystem fs = FileSystem.get(new Configuration());
-        BufferedReader br=new BufferedReader(new InputStreamReader(fs.open(pt)));
-        String line;
-        String tuple[];
-        Integer list[] = new Integer[3];
-        int count = 1;
+    public void setup(Context context) throws IOException, InterruptedException{
+        Path pt = new Path("./p4_dataset/KSeed.csv");     //Location of file in HDFS
+        String thepath;
+        Path pt1;
         
-        while ((line=br.readLine())!=null){
-            tuple = line.split(",");
-            if(tuple.length==3){
-                list[0] = Integer.parseInt(tuple[1]);
-                list[1] = Integer.parseInt(tuple[1]);
-                list[2] = Integer.parseInt(tuple[2]);
-                seedlist.add(list);  //custID + countryCode
-                count++;
+        FileSystem fs = FileSystem.get(new Configuration());
+        
+        for(int n=0; n<6; n++){
+            thepath = "./KMeans_out" + n + "/part-r-00000";
+            pt1 = new Path(thepath);
+            if(fs.exists(pt1)){
+                pt = new Path(thepath);
+            }else{
+                break;
             }
         }
         
+        BufferedReader br=new BufferedReader(new InputStreamReader(fs.open(pt)));
+        String line;
+        String tuple1[];
+        int kid = 0;
+        
+        while ((line=br.readLine())!=null){
+            line = line.replaceAll("\\s", "");
+            tuple1 = line.split(",");
+            if(tuple1.length==3){
+                Integer list[] = new Integer[2];
+                kid = Integer.parseInt(tuple1[0]);
+                list[0] = Integer.parseInt(tuple1[1]);
+                list[1] = Integer.parseInt(tuple1[2]);
+                seedpoints.put(kid, list);  //need to open here
+            }
+        }
     }
     
     public void map(Object key, Text value, Context context
                     ) throws IOException, InterruptedException {
-      StringTokenizer itr = new StringTokenizer(value.toString());
+      String prevalue = value.toString().replaceAll("\\s", "");
+      StringTokenizer itr = new StringTokenizer(prevalue);
       String token;
-      String tuple[];
-      int point[] = new int[2];
-      float cmin = 0;
-      float min_dis = Float.MAX_VALUE;
       int kmeansID = 1;
+      String tuple[];
+      float cmin1;
+      float min_dis1 = Float.MAX_VALUE;
+      //int kcount = 0;
       
       while (itr.hasMoreTokens()) {
         token = itr.nextToken();
-//        tuple = token.split(",");
-        seedID.set(kmeansID);
-        inpoint.set(token);
-        context.write(seedID, inpoint);
+        tuple = token.split(",");
         
-      }
-    }
-  }
-  
-  public static class TransSumReducer 
-       extends Reducer<IntWritable, Text , IntWritable , Text>{
-    private Text result = new Text();
-    private IntWritable outseed = new IntWritable();
-    private int itrcount = 0;
-    private Map<Integer, Integer[]> seedpoints = new HashMap<Integer, Integer[]>();
-    Map<Integer, ArrayList<Integer[]>> pointclust = new HashMap<Integer, ArrayList<Integer[]>>();
-    private Iterable<Text> savedVal;
-    private Context savedCont;
-    private IntWritable savedKey;
-    
-    public void reduce(IntWritable key, Iterable<Text> values, 
-                       Context context
-                       ) throws IOException, InterruptedException {
-      savedKey = key;
-      savedVal = values;
-      savedCont = context;
-      String info;
-      String tuples[];
-      int x = 0;
-      int y = 0;
-      int sumofX = 0;
-      int sumofY = 0;
-      int countp = 0;
-      int avgX = 0;
-      int avgY = 0;
-      boolean flag = false;
-      float cmin;
-      float min_dis;
-      int clustID = 1;
-      boolean endflag = false; //to terminate iteration of reducer
-      
-      if(seedpoints.isEmpty()){
-          flag = true; //check if it is the first time to enter reducer
-      }
-      
-      /**
-       * Here is to calculate smallest distant and group the points to k-means
-       */
-      for (Text val : values) {
-         info = val.toString();
-         tuples = info.split(",");
-         cmin = Float.MAX_VALUE;  
-         min_dis = Float.MAX_VALUE; //saved current min distance
-         
-         if(tuples.length == 3){   //this is from seed points
-             if(flag){   //this only execute once when first time run reducer
-                Integer tempoint[] = new Integer[2];
-                ArrayList<Integer[]> thepoint = new ArrayList<Integer[]>();
-                clustID = Integer.parseInt(tuples[0]);
-                tempoint[0] = Integer.parseInt(tuples[1]);  //seed x point
-                tempoint[1] = Integer.parseInt(tuples[2]);  //seed y point
-                seedpoints.put(clustID, tempoint); //has saved to local memory now
-                pointclust.put(clustID, thepoint); //has saved to local memory now
-             }
-             // else case just ignore original seed points
-             // here is the test for output
-             outseed.set(clustID);
-             result.set(info);
-             context.write(outseed, result);
-         }else if(tuples.length == 2){   //this is from test points
-            x = Integer.parseInt(tuples[0]);  //x1
-            y = Integer.parseInt(tuples[1]);  //y1
-            Integer savepoint[] = new Integer[2];
-            savepoint[0] = x;
-            savepoint[1] = y;
-            Integer oneseed[];
-            
-            for(int i=1; i<=seedpoints.size(); i++){
-                oneseed = seedpoints.get(i);
-                cmin = distance(oneseed, x, y);
-                if(cmin < min_dis){
-                    min_dis = cmin;
-                    clustID = i;
-                }
+        if(tuple.length == 3){
+            if(tuple[0]==""){
+                continue;
             }
-            
-            ArrayList<Integer[]> temp = pointclust.get(clustID);
-            if(temp==null){
-                //nothing to do
-            } else{
-                temp.add(savepoint);
-                pointclust.put(clustID, temp);  //save group of points into the map
-                // here is to test output reslut
-//                outseed.set(clustID);
-//                result.set(info);
-//                context.write(outseed, result);
-             }
-         }
-      }
-      
-      /**
-       * here is to recompute the k-means position
-       **/
-      
-      ArrayList<Integer[]> somepoints = new ArrayList<Integer[]>();
-      Integer orig_seed[];
-      for(int i=1; i<=pointclust.size(); i++){  //recomput the k-means position
-          somepoints = pointclust.get(i);
-          sumofX = 0;
-          sumofY = 0;
-          countp = 0;
-          
-          if(somepoints==null){
-              continue;
-          }
-          
-          for(Integer[] onepoint: somepoints){
-              sumofX += onepoint[0];
-              sumofY += onepoint[1];
-              countp++;
-          }
-          
-          if(countp!=0){
-            avgX = sumofX/countp;
-            avgY = sumofY/countp;
-          }else{
-              continue;
-          }
-          
-          orig_seed = seedpoints.get(i);
-          if(orig_seed == null){
-              continue;
-          }
-          
-          if(itrcount>5){  //do no more than 6 iteration
-              endflag = true;
-          }else if(Math.abs(orig_seed[0]-avgX) > 10 || Math.abs(orig_seed[1]-avgY) > 10){ //threshold=3
-              orig_seed[0] = avgX;
-              orig_seed[1] = avgY;
-              seedpoints.put(i, orig_seed);  //update the new k-means position
-              endflag = false;
-          }else{
-              endflag = true;
-          }
-      }
-      
-      Integer outp[] = new Integer[2];
-      
-      if(endflag){  //iteration will be terminate here
-        for(int m=1; m<=seedpoints.size(); m++){
-            outseed.set(m);
-            outp = seedpoints.get(m);
-            result.set("Centroid: " + outp[0] + "," + outp[1]);
-            context.write(outseed, result);  //output the last k-means centroid
-        }
-      }else{    
-        //tail iteration call reducer here, increment counter
-        itrcount++;
-        reduce(savedKey, savedVal, savedCont);  //there is problem here.
-      }
-     
-    }
+            Integer list[] = new Integer[2];
+            kmeansID = Integer.parseInt(tuple[0]);
+            list[0] = Integer.parseInt(tuple[1]);
+            list[1] = Integer.parseInt(tuple[2]);
+            seedpoints.put(kmeansID, list);
+            //kmeansID = Integer.parseInt(tuple[0]);
+            seedID.set(kmeansID);    //centroid seed ID
+            inpoint.set(token);    //centroid x y position 
+            context.write(seedID, inpoint);  //write centroid points to reducer
+        }else if(tuple.length == 2){
+            if(seedpoints.isEmpty()){
+                //do nothing
+            }else{
+                int x1 = Integer.parseInt(tuple[0]);  //x1
+                int y1 = Integer.parseInt(tuple[1]);  //y1
+                Integer oneseed[];
 
+                for(int n=1; n<=seedpoints.size(); n++){
+                    oneseed = seedpoints.get(n);
+                    if(oneseed == null){
+                        // do nothing
+                    }else{
+                        cmin1 = distance(oneseed, x1, y1);
+                        if(cmin1 < min_dis1){
+                            min_dis1 = cmin1;
+                            kmeansID = n;
+                        }
+                    }
+                }
+                
+                min_dis1 = Float.MAX_VALUE;
+                seedID.set(kmeansID); 
+                inpoint.set(token);
+                context.write(seedID, inpoint); //write the test points to reducer
+            }
+        }
+      }
+      
+      flagin = false; //the mapper won't read the original centroid again
+    }
+    
     public float distance(Integer[] seed, int x, int y){
         float r;
         int kx = seed[0];
@@ -259,29 +155,150 @@ public class KMeansClustering{
     }
   }
   
+  public static class KMeansReducer 
+       extends Reducer<IntWritable, Text , IntWritable , Text>{
+    private Text result = new Text();
+    private IntWritable outseed = new IntWritable();
+    private Map<Integer, Integer[]> pointclust = new HashMap<Integer, Integer[]>();
+    private Iterable<Text> savedVal;
+    private Context savedCont;
+    private IntWritable savedKey;
+    
+    public void reduce(IntWritable key, Iterable<Text> values, Context context
+                       ) throws IOException, InterruptedException {
+      savedKey = key;
+      savedVal = values;
+      savedCont = context;
+      String info;
+      String tuples[];
+      int sumofX = 0;
+      int sumofY = 0;
+      int countp = 0;
+      int x;
+      int y;
+      int avgX;
+      int avgY;
+      int clustID = Integer.parseInt(key.toString());
+      Integer savedTuple[] = new Integer[2];
+      savedTuple[0] = 1;
+      savedTuple[1] = 1;
+      
+      /**
+       * Here is to calculate smallest distant and group the points to k-means
+       */
+      for (Text val : values) {
+         info = val.toString();
+         info = info.replaceAll("\\s", "");
+         tuples = info.split(","); 
+      
+         if(tuples.length == 3){   //this is from seed points
+             clustID = Integer.parseInt(tuples[0]);
+             savedTuple[0] = Integer.parseInt(tuples[1]);  //save the original x
+             savedTuple[1] = Integer.parseInt(tuples[2]);  //save the original y
+             Integer newSeed[] = new Integer[2];
+             newSeed[0] = savedTuple[0];
+             newSeed[1] = savedTuple[1];
+             pointclust.put(clustID, newSeed);
+//             outseed = key;
+//             result.set("old centroid: " + tuples[1] + "," + tuples[2]);
+//             context.write(outseed, result);
+         }else if(tuples.length == 2){   //this is from test points
+            x = Integer.parseInt(tuples[0]);  //x1
+            y = Integer.parseInt(tuples[1]);  //y1
+            
+            sumofX += x;
+            sumofY += y;
+            countp++;
+         }
+      }
+      
+      if(countp!=0){
+        avgX = sumofX/countp;
+        avgY = sumofY/countp;
+//        savedTuple = seedpoints.get(clustID);
+        if(savedTuple==null){
+            //do nothing
+        }else{
+            if((avgX - savedTuple[0] > 5) || (avgY - savedTuple[1] > 5)){ //treshold
+                clustID = Integer.parseInt(key.toString());
+                Integer[] ttp = pointclust.get(clustID);  
+                ttp[0] = avgX;
+                ttp[1] = avgY;
+                pointclust.put(clustID, ttp);  //update the new seedpoints
+                outseed.set(clustID);
+                result.set("," + avgX + "," + avgY);
+                context.write(outseed, result);  //write out to next MapReduce
+            }
+        }
+      }
+      else if(countp==0){  //if there is no closest points
+          Integer[] ttp1 = pointclust.get(clustID);
+//          seedpoints.put(clustID, ttp1);
+          outseed.set(clustID);
+          result.set("," + ttp1[0] + "," + ttp1[1]);
+          context.write(outseed, result);
+      }
+      
+//      for(int k=0; k<pointclust.size();k++){
+//          Integer[] temp = pointclust.get(k);
+//          if(temp==null){
+//              continue;
+//          }else{
+//              outseed.set(k);
+//              result.set(","+temp[0]+","+temp[1]);
+//              context.write(outseed, result);
+//          }
+//      }
+      
+    } //end of reducer function
+  }
+  
   public static void main(String[] args) throws Exception {
      Configuration conf = new Configuration(false);
+     int n = 1;
      
      if (args.length != 3) {
       System.err.println("Usage: KMeansClustering <HDFS input file1> <HDFS input file2> <HDFS output file>");
       System.exit(2);
     }
     
-    Job job = new Job(conf, "K_Means");
+    Job job = new Job(conf, "K_Means_"+0);
     job.setJarByClass(KMeansClustering.class);
-    job.setMapperClass(TokenizerMapper.class);
+    job.setMapperClass(KMeansMapper.class);
     job.setMapOutputKeyClass(IntWritable.class);
     job.setMapOutputValueClass(Text.class);
     //job.setCombinerClass(TransSumReducer.class);
-    job.setReducerClass(TransSumReducer.class);
+    job.setReducerClass(KMeansReducer.class);
     job.setNumReduceTasks(1);
     job.setOutputKeyClass(IntWritable.class);
     job.setOutputValueClass(Text.class);
     
-    MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, TokenizerMapper.class);
-    MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, TokenizerMapper.class);
+    MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, KMeansMapper.class);
+    MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, KMeansMapper.class);
     FileOutputFormat.setOutputPath(job, new Path(args[2]));
+    
+    if(job.waitForCompletion(true)){
+        while(job.waitForCompletion(true) && n<3){
+            Configuration conf1 = new Configuration(false);
+            job = new Job(conf1, "K_Means_"+n);
+            job.setJarByClass(KMeansClustering.class);
+            job.setMapperClass(KMeansMapper.class);
+            job.setMapOutputKeyClass(IntWritable.class);
+            job.setMapOutputValueClass(Text.class);
+            //job.setCombinerClass(TransSumReducer.class);
+            job.setReducerClass(KMeansReducer.class);
+            job.setNumReduceTasks(1);
+            job.setOutputKeyClass(IntWritable.class);
+            job.setOutputValueClass(Text.class);
+
+            MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, KMeansMapper.class);
+            MultipleInputs.addInputPath(job, new Path("./KMeans_out"+(n-1)+"/part-r-00000"), TextInputFormat.class, KMeansMapper.class);
+            FileOutputFormat.setOutputPath(job, new Path("./KMeans_out"+n));
+            n++;
+        }
+    }
     System.exit(job.waitForCompletion(true) ? 0 : 1);
+    
   }
   
 }
--- hw4/run3.sh
+++ hw4/run3.sh
@@ -5,7 +5,11 @@ packname="org.apache.hadoop.examples.KMeansClustering"
 jarfile="KMeansClustering.jar"
 input1="./p4_dataset/2DPoints.csv"
 input2="./p4_dataset/KSeed.csv"
-output="./KMeans_out"
+output="./KMeans_out0"
+output1="./KMeans_out1"
+output2="./KMeans_out2"
 
 hadoop fs -rmr $output
+hadoop fs -rmr $output1
+hadoop fs -rmr $output2
 hadoop jar ./$jarfile $packname $input1 $input2 $output
