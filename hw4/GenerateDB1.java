ac13022b9ba5f195ec0c7d5435383b8dcc454406
Emmagitlab <emma2014yang@hotmail.com>
2016-03-30 14:44:44 -0400

    Added files via upload

--- hw4/GenerateDB1.java
+++ hw4/GenerateDB1.java
@@ -1,87 +1,63 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Random;
 
 /**
- * CS561 Project 4
- * Team 10
- * Date Mar/25/2016.
+ * Created by T440 on 2016/3/26.
  */
-
 public class GenerateDB1 {
+    private int x=0;
+    private int y=0;
+    Random r = new Random();
+    private String points = null;
+    private String rectagle = null;
 
-	private int px = 0;
-	private int py = 0;
-    private int rectangle_x1 = 0;
-    private int rectangle_y1 = 0;
-    private int rectangle_x2 = 0;
-    private int rectangle_y2 = 0;
-    private int height = 0;
-    private int width = 0;
-    private String p_record = null;
-    private String r_record = null;
 
-   // private String transactionFilePath = null;
-
-    public static void main (String arg[]) throws IOException {
-    	System.out.println("start creating P datasets and R datasets. . . . .");
+    public static void main(String arg[]) throws IOException {
+        System.out.println("start creating two datasets . . . . .");
         GenerateDB1 db = new GenerateDB1();
         db.createFile();
         System.out.println("Complete task!!!");
+
     }
 
     public void createFile() throws IOException {
-        String FilePathP = "PDataset.csv";
-        String FilePathR = "RDataset.csv";
+        String filename1 = "DB_P.csv";
+        String filename2 = "DB_R.csv";
+        FileWriter writer1 = new FileWriter (filename1);
+        FileWriter writer2 = new FileWriter(filename2);
 
-        File cfile = new File(FilePathP);
-        File tfile = new File(FilePathR);
-        FileWriter cWriter = new FileWriter(FilePathP);
-        FileWriter tWriter = new FileWriter(FilePathR);
+        for (int i = 0; i < 15000000; i++) {
+            int x = randomInt(10001, 1);
+            int y = randomInt(10001, 1);
+            points = x + "," + y;
+            writer1.append(points);
+            writer1.append('\n');
 
-        for(int i = 1; i <= 100; i++ ){   //total 9,000,000 lines to generate 100MB
-            px = randomInt(1, 100001);  //x and y range from 1 to 100000
-            py = randomInt(1, 100001);
-            p_record = px + "," + py;
-
-            cWriter.append(p_record);
-            cWriter.append('\n');
         }
-        
-        cWriter.flush();
-        cWriter.close();
-        
-        for(int j = 1; j <= 40; j++ ) {   //total 4,600,000 lines to generate 100MB
-            rectangle_x1 = randomInt(1, 100001);
-            rectangle_y1 = randomInt(1, 100001);
-            rectangle_x2 = rectangle_x1 + randomInt(1, 56); //rectange height and width range
-            rectangle_y2 = rectangle_y1 + randomInt(1, 56);
-            
-            if(rectangle_x2 > 100000){  //check x value for exceeding outside range
-            	rectangle_x2 = 100000;
-            }
-            
-            if(rectangle_y2 > 100000){  //check y value for exceeding outside range
-            	rectangle_y2 = 100000;
-            }
-            
-            r_record = rectangle_x1 + "," + rectangle_y1 + "," +  rectangle_x2 + "," + rectangle_y2;
+        writer1.flush();
+        writer1.close();
+        for (int i = 0; i < 5000000; i++) {
+            String R = "r" + i;
+            int top_left_x = randomInt(10001, 1);
+            int top_left_y = randomInt(10001, 1);
+            int height = randomInt(21,1);
+            int width = randomInt(6,1);
+            rectagle = R +","+top_left_x + "," + top_left_y + "," + height + "," + width ;
+            writer2.append(rectagle);
+            writer2.append('\n');
 
-            tWriter.append(r_record);
-            tWriter.append('\n');
         }
-        tWriter.flush();
-        tWriter.close();
-
+        writer2.flush();
+        writer2.close();
     }
 
-    public static int randomInt(int min, int max){
-    	int num = (int) (Math.random() * (max - min)) + min;
+    public static int randomInt( int max, int min){
+        int num = (int) (Math.random() * (max - min)) + min;
         return num;
     }
+
 }
-
-
-
 
 
--- hw4/run1.sh
+++ hw4/run1.sh
@@ -5,7 +5,7 @@ packname="org.apache.hadoop.examples.SpatialJoin"
 jarfile="SpatialJoin.jar"
 input1="./p4_dataset/DB_R.csv"
 input2="./p4_dataset/DB_P.csv"
-output="./spatialJoin_out"
+output="./project1"
 
 hadoop fs -rmr $output
 hadoop jar ./$jarfile $packname $input1 $input2 $output
