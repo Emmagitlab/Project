378e9a7566b1f7af8e96d8ec6fc059d4c8ca822a
Fangyu Lin <johnlin989@gmail.com>
2016-03-30 13:34:14 -0400

    Update run1.sh

--- hw4/run1.sh
+++ hw4/run1.sh
@@ -5,7 +5,7 @@ packname="org.apache.hadoop.examples.SpatialJoin"
 jarfile="SpatialJoin.jar"
 input1="./p4_dataset/DB_R.csv"
 input2="./p4_dataset/DB_P.csv"
-output="./project1"
+output="./spatialJoin_out"
 
 hadoop fs -rmr $output
 hadoop jar ./$jarfile $packname $input1 $input2 $output
